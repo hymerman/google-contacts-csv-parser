3c643754ab22084add7fce75f5d4a059d23cf57a
Ben Hymers <b.hymers@gmail.com>
2014-11-04 23:25:39 +0000

    Comment out verbose log statements

--- google-contacts-csv-parser.js
+++ google-contacts-csv-parser.js
@@ -41,16 +41,16 @@ var parser = parse({delimiter: ',', quote:'"', columns:true}, function(err, data
             var property_which_should_be_array = properties_which_should_be_arrays[property_which_should_be_array_index];
             //console.log("Checking " + property_which_should_be_array);
             if(property_name.indexOf(property_which_should_be_array) === 0) {
-              console.log("Found field which should be an array: " + property_name);
+              //console.log("Found field which should be an array: " + property_name);
               // These fields are all of the form: "Field Name N - Inner Field Name", where both fields can have spaces. I think the best way to get both field names is to split on the number and hyphen.
               var regex = /(\d+)[ ]-[ ](.*)/;
               var result = property_name.substring(property_which_should_be_array.length + 1).match(regex);
               var array_index = result[1] - 1;
               var sub_object_property_name = result[2];
-              console.log("Extracted array index: " + array_index + ", property name: " + sub_object_property_name);
+              //console.log("Extracted array index: " + array_index + ", property name: " + sub_object_property_name);
               if(contact[property_which_should_be_array] === undefined) {
                 // Array doesn't exist yet. Create it!
-                console.log("Array doesn't exist yet. Creating it.");
+                //console.log("Array doesn't exist yet. Creating it.");
                 console.assert(array_index === 0);
                 contact[property_which_should_be_array] = [{}];
               } else if(!(contact[property_which_should_be_array] instanceof Array)) {
@@ -58,12 +58,12 @@ var parser = parse({delimiter: ',', quote:'"', columns:true}, function(err, data
                 throw Error("Array field already exists, but not as array!");
               } else if(contact[property_which_should_be_array].length <= array_index) {
                 // Array exists but we've come to a new item - append it to the array.
-                console.log("Array exists but we've come to a new item - appending it to the array.");
+                //console.log("Array exists but we've come to a new item - appending it to the array.");
                 console.assert(array_index === contact[property_which_should_be_array].length);
                 contact[property_which_should_be_array].push({});
               } else {
                 // Array exists, and object at this index exists; add data to it.
-                console.log("Array exists, and object at this index exists; adding data to it.");
+                //console.log("Array exists, and object at this index exists; adding data to it.");
               }
               contact[property_which_should_be_array][array_index][sub_object_property_name] = contact[property_name];
               delete contact[property_name];
